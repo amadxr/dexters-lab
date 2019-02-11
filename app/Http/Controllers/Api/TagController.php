cb1d86d75e0afa10118aec9781914323e49b20f1
Amador <joseamadorzn@gmail.com>
2019-02-11 17:49:55 -0500

    Filter tags in index method
    Takes key sent in get request and uses it to filter tags in the index
    method of the TagController. Also creates tags seeder in order to test
    api.

--- app/Http/Controllers/Api/TagController.php
+++ app/Http/Controllers/Api/TagController.php
@@ -9,9 +9,18 @@ use Illuminate\Http\Request;
 
 class TagController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
         $tags = Tag::all();
+
+        if (isset($request->textInput)) {
+            $filteredTags = $tags->filter(function ($tag) use ($request) {
+                $substring = substr($tag->name, 0, strlen($request->textInput));
+                return $substring == $request->textInput;
+            });
+
+            $tags = $filteredTags;
+        }
 
         return TagResource::collection($tags);
     }
